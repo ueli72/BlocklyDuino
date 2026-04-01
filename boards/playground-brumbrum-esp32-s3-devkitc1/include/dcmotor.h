3372b0307dd1ebed71f133cb360b00688177df0c
Ueli Leutwyler <ueli@rumlikon.ch>
2026-04-01 07:09:26 +0200

    dc motors adjusted

--- boards/playground-brumbrum-esp32-s3-devkitc1/include/dcmotor.h
+++ boards/playground-brumbrum-esp32-s3-devkitc1/include/dcmotor.h
@@ -8,14 +8,14 @@
 // Module 1
 #define MOTOR1_IN1_PIN 15   // Changed from 0 to 5 (GPIO0 is boot pin, can cause issues)
 #define MOTOR1_IN2_PIN 16   // IN2: PWM or high/low
-#define MOTOR2_IN1_PIN 43  // IN3
-#define MOTOR2_IN2_PIN 44  // IN4
+#define MOTOR2_IN1_PIN 12  // IN3
+#define MOTOR2_IN2_PIN 13  // IN4
 
 // Module 2
 #define MOTOR3_IN1_PIN 0  // IN1
 #define MOTOR3_IN2_PIN 7  // IN2
-#define MOTOR4_IN1_PIN 12  // IN3
-#define MOTOR4_IN2_PIN 13  // IN4
+#define MOTOR4_IN1_PIN 43  // IN3
+#define MOTOR4_IN2_PIN 44  // IN4
 
 // Directions
 #define FORWARD 1
